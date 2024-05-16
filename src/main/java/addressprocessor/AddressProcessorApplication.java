48228af84adf1381b0f2010effe7e7a3cc35057b
Marcos Winicios Pereira Martins <mwmartins1@topaz.com.br>
2024-05-16 01:38:10 -0300

    init: Create project

---
+++ src/main/java/addressprocessor/AddressProcessorApplication.java
@@ -0,0 +1,13 @@
+package addressprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressProcessorApplication.class, args);
+	}
+
+}
