a7e821350874d987f962e0abc27eff1fad73ddbb
Marcos Winicios Pereira Martins <mwmartins1@topaz.com.br>
2024-05-16 01:47:27 -0300

    feat: Creating class with utility methods for processing csv files

---
+++ src/main/java/addressprocessor/utils/CsvUtil.java
@@ -0,0 +1,84 @@
+package addressprocessor.utils;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvException;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+@Component
+public class CsvUtil {
+
+    private static final String BASE_PATH = "inputFiles/";
+
+    /**
+     * @param data          Informações que irão compor o conteúdo do arquivo CSV
+     * @param directoryPath Diretório alvo onde o arquivo será salvo
+     * @param fileName      Nome do arquivo a ser gerado
+     */
+    private static String generateCsvFile(List<String[]> data, String directoryPath, String fileName) {
+
+        try {
+            String fileFullName = directoryPath + fileName + ".csv";
+
+            File directory = new File(fileFullName).getParentFile();
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+
+            // Criar o arquivo se não existir
+            File file = new File(fileFullName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(fileFullName);
+            CSVWriter cw = new CSVWriter(fw);
+
+            cw.writeAll(data);
+
+            System.err.println("\nArquivo CSV gerado com sucesso em: " + file.getAbsolutePath() + "\n");
+
+            cw.close();
+            fw.close();
+            return file.getAbsolutePath();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public static List<String[]> readCsvFile(String fileName) {
+
+        String pathName = BASE_PATH + fileName;
+
+        try (CSVReader reader = new CSVReader(new FileReader(pathName))) {
+
+            List<String[]> lines = reader.readAll();
+            return lines;
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void printCsvFile(String fileName){
+        printCsvLines(Objects.requireNonNull(readCsvFile(fileName)));
+    }
+
+    public static void printCsvLines(List<String[]> lines){
+        for (String[] line : lines) {
+            for (String field : line) {
+                System.out.print(field + " | ");
+            }
+            System.out.println(); // Pula para a próxima linha
+        }
+    }
+
+}
