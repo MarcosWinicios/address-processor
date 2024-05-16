7812ca2e38c5f9aca1af2ed3320dc51059e450b0
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-05-16 01:38:10 -0300

    init: Create project

---
+++ src/test/java/addressprocessor/AddressProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package addressprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
