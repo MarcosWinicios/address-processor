cf3455112b68478758df2ee644fdb0f1cfd6f2cd
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-05-18 02:56:44 -0300

    feat: Adding reading from neighborhood list csv file

---
+++ src/main/java/addressprocessor/dto/input/NeighborhoodExternalInputDTO.java
@@ -0,0 +1,41 @@
+package addressprocessor.dto.input;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NeighborhoodExternalInputDTO {
+
+    //TZ_LOCK,DIM1,DIM2,DIM3,NOMBRE,CODIGOPOSTAL,ASENTAMIENTO,CIUDAD,CODIGOPAIS,ZONAFISCAL,MIN_NOMBRE
+    private Integer id;
+    private String name;
+    private String externalCode;
+    private Integer stateExternalCode;
+    private Integer cityExternalCode;
+    private String countryCode;
+
+
+    public NeighborhoodExternalInputDTO(String[] line) {
+        this.name = line[4];
+        this.externalCode = line[3];
+        this.stateExternalCode = Integer.parseInt(line[1]);
+        this.cityExternalCode = Integer.parseInt(line[2]);
+        this.countryCode = line[8];
+
+    }
+
+    @Override
+    public String toString() {
+        return "NeighborhoodExternalInputDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", externalCode='" + externalCode + '\'' +
+                ", stateExternalCode=" + stateExternalCode +
+                ", cityExternalCode=" + cityExternalCode +
+                ", countryCode='" + countryCode + '\'' +
+                '}';
+    }
+}
--- src/main/java/addressprocessor/tests/ReadCsvFileCityExternal.java
+++ src/main/java/addressprocessor/tests/ReadCsvFileCityExternal.java
@@ -1,9 +1,11 @@
 package addressprocessor.tests;
 
 import addressprocessor.dto.input.CityExternalInputDTO;
+import addressprocessor.dto.input.NeighborhoodExternalInputDTO;
 import addressprocessor.dto.input.StateInputDTO;
 import addressprocessor.model.City;
 import addressprocessor.service.CityService;
+import addressprocessor.service.NeighborhoodService;
 import addressprocessor.service.StateService;
 
 import java.util.List;
@@ -34,10 +36,21 @@ public class ReadCsvFileCityExternal {
 
         System.out.println("cityExternalInputDTOList: " + cityExternalInputDTOList.size());
 
-        cityService.printCityExternalObj(cityExternalInputDTOList);
+//        cityService.printCityExternalObj(cityExternalInputDTOList);
 
         System.out.println("\n_______________________________________________\n");
         System.out.println("Lendo arquivo com os bairros\n");
+        String neighborhoodName = "tb_neighborhood_external.csv";
+
+        NeighborhoodService neighborhoodService = new NeighborhoodService();
+        var neighborhoodLines = neighborhoodService.readCsvFile(neighborhoodName);
+        List<NeighborhoodExternalInputDTO> neighborhoodExternalInputDTOList = neighborhoodService.csvToNeighborhoodExternalObject(neighborhoodLines);
+
+        System.out.println("neighborhoodExternalInputDTO: " + neighborhoodExternalInputDTOList.size());
+
+        neighborhoodService.printNeighborhoodExternalInputDTOObj(neighborhoodExternalInputDTOList);
+
+        System.out.println("FIM");
 /*
         //--------------------
         List<City> cityList =  cityService.relateCitiesToStates(cityExternalInputDTOList, stateInputDTOList);
