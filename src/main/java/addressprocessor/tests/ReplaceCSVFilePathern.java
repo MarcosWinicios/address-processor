d622c8cd71760ee26012fbf76802e9c1361af6e9
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-07-06 20:30:44 -0300

    feat: Creating resource to change csv file default

--- src/main/java/addressprocessor/tests/ReplaceCSVFilePathern.java
+++ src/main/java/addressprocessor/tests/ReplaceCSVFilePathern.java
@@ -8,20 +8,20 @@ public class ReplaceCSVFilePathern {
 	
 	public static void main(String[] args) {
         // Caminho do arquivo CSV existente
-        String inputFileName = "neighborhood_relation.csv";
+        String inputFileName = "TC_ACTIVIDADES_BANXICO.csv";
         
         // Lê o arquivo CSV existente
         System.out.println("Lendo arquivo");
-//        List<String[]> data = CsvUtil.readCsvFileWithoutQuotes(inputFileName);
-        List<String[]> data = CsvUtil.readCsvFile(inputFileName);
+        List<String[]> data = CsvUtil.readCsvFileWithoutQuotes(inputFileName);
+//        List<String[]> data = CsvUtil.readCsvFile(inputFileName);
 
         // Caminho do diretório e nome do novo arquivo
         
-        String outputFileName = "new_neighborhood_relation";
+        String outputFileName = "NEW_" + inputFileName;
 
         // Gera o novo CSV com o formato especificado
         System.out.println("Gerando arquivo");
-        CsvUtil.generateCsvFile(data, CsvUtil.getOutputBasePath(), outputFileName);
+        CsvUtil.generateCsvFile(data, CsvUtil.getOutputBasePath(), outputFileName.replace(".csv", ""));
         
         System.out.println("FIM");
     }
