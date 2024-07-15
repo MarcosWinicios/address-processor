3db668ee7a7be3ed98db6f0f45562a52580eeb5e
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-07-14 21:07:20 -0300

    feat: Adding method to identify separator and delimiter of CSV files

--- src/main/java/addressprocessor/utils/CsvUtil.java
+++ src/main/java/addressprocessor/utils/CsvUtil.java
@@ -4,15 +4,10 @@ import com.opencsv.*;
 import com.opencsv.exceptions.CsvException;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.io.*;
+import java.util.*;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @Component
 public class CsvUtil {
@@ -103,14 +98,21 @@ public class CsvUtil {
         return null;
     }
 
-    public static List<String[]> readCsvFileMX(String fileName){
+    /**
+     *
+     * @param fileName Input CSV file name
+     * @param filterValue Value to be used in the filter.
+     * @param indexPosition Position of the column where the <b>filterValue</b> value is located
+     * @return
+     */
+    public static List<String[]> readCsvFileWithFilterSemicolonSingleQuotes(String fileName, String filterValue, int indexPosition ){
 
         System.out.println("Lendo arquivo: " + fileName);
         String pathName = INPUT_BASE_PATH + fileName;
 
         // Configura o parser para usar ; como separador e ' como delimitador
         CSVParser parser = new CSVParserBuilder()
-                .withSeparator(';')
+                .withSeparator(',')
                 .withQuoteChar('\'')
                 .build();
 
@@ -123,21 +125,118 @@ public class CsvUtil {
             String[] nextLine;
             int count = 0;
             while ((nextLine = reader.readNext()) != null) {
-                if(count == 0){
-                    filteredLines.add(nextLine);
+                if(count == 0){ // Header File
+                    printLine(nextLine);
+//                    filteredLines.add(nextLine);
                     count++;
                 }
                 // Verifica se o valor na coluna especificada corresponde ao valor de filtro
-                if (nextLine[8].equals("MX")) {
+                if (nextLine[indexPosition].equals(filterValue)) {
                     filteredLines.add(nextLine);
                 }
             }
+
             return filteredLines;
         } catch (IOException | CsvException e) {
             e.printStackTrace();
         }
         return null;
     }
+
+    private static String removeFileExtension(String fileName){
+        int dotIndex = fileName.lastIndexOf('.');
+
+        if (dotIndex > 0 && dotIndex < fileName.length() - 1) {
+            return fileName.substring(0, dotIndex);
+        } else {
+            return fileName; // Retorna o nome completo se não houver ponto ou se estiver no início/final
+        }
+    }
+
+    public static List<String[]> readCsvFileGenericFormat(String fileName, String filterValue, int indexPosition){
+        fileName = removeFileExtension(fileName) + ".csv";
+        System.out.println("Lendo arquivo: " + fileName);
+
+        String pathName = INPUT_BASE_PATH + fileName;
+
+        Map<String, Character> separatorAndDelimiter = detectDelimiterAndSeparator(fileName);
+
+        printFileFormat(separatorAndDelimiter);
+        // Configura o parser para usar ; como separador e ' como delimitador
+        CSVParser parser = new CSVParserBuilder()
+                .withSeparator(separatorAndDelimiter.get("separator"))
+                .withQuoteChar(separatorAndDelimiter.get("delimiter"))
+                .build();
+
+        List<String[]> filteredLines = new ArrayList<>();
+
+        try (CSVReader reader = new CSVReaderBuilder(new FileReader(pathName))
+                .withCSVParser(parser)
+                .build()) {
+
+            String[] nextLine;
+            int count = 0;
+            while ((nextLine = reader.readNext()) != null) {
+                if(count == 0){ // Header File
+                    printLine(nextLine);
+                    filteredLines.add(nextLine);
+                    count++;
+                }
+                // Verifica se o valor na coluna especificada corresponde ao valor de filtro
+                if (nextLine[indexPosition].equals(filterValue)) {
+                    filteredLines.add(nextLine);
+                }
+            }
+
+            return filteredLines;
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Map<String, Character> detectDelimiterAndSeparator(String fileName) {
+        String pathName = INPUT_BASE_PATH + fileName;
+        char[] possibleSeparators = {',', ';', '\t', '|', ' '};
+        char[] possibleDelimiters = {'"', '\'', ' '};
+        Map<String, Character> result = new HashMap<>();
+        Map<Character, Integer> separatorCounts = new HashMap<>();
+        Map<Character, Integer> delimiterCounts = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(pathName))) {
+            String line = br.readLine(); // Lê a primeira linha para análise
+
+            if (line != null) {
+                // Conta a frequência de cada possível separador na linha
+                for (char sep : possibleSeparators) {
+                    int count = line.length() - line.replace(String.valueOf(sep), "").length();
+                    separatorCounts.put(sep, count);
+                }
+
+                // Conta a frequência de cada possível delimitador na linha
+                for (char delim : possibleDelimiters) {
+                    int count = line.length() - line.replace(String.valueOf(delim), "").length();
+                    delimiterCounts.put(delim, count);
+                }
+
+                // Determina o separador mais provável (maior contagem)
+                char probableSeparator = separatorCounts.entrySet().stream()
+                        .max(Map.Entry.comparingByValue()).get().getKey();
+                result.put("separator", probableSeparator);
+
+                // Determina o delimitador mais provável (maior contagem), excluindo espaço em branco
+                char probableDelimiter = delimiterCounts.entrySet().stream()
+                        .filter(entry -> entry.getKey() != ' ')
+                        .max(Map.Entry.comparingByValue()).get().getKey();
+                result.put("delimiter", probableDelimiter);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
 
     public static List<String[]> readCsvFileWithoutQuotes(String fileName){
 
@@ -169,6 +268,9 @@ public class CsvUtil {
         }
     }
 
+    public static void printLine(String[] line){
+        System.out.println(String.join(", ", line));
+    }
     public static void printCsvLines(List<String[]> lines){
 
 
@@ -193,5 +295,7 @@ public class CsvUtil {
 
          */
     }
-
+    public static  void printFileFormat(Map<String, Character> fileFormat){
+        System.err.println(fileFormat);
+    }
 }
