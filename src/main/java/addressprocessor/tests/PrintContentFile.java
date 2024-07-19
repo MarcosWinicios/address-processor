e8985ce427e69c4cfc3afd4a32dd65c16f9b804c
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-07-19 00:21:39 -0300

    fix: Fix print content file Method

--- src/main/java/addressprocessor/tests/PrintContentFile.java
+++ src/main/java/addressprocessor/tests/PrintContentFile.java
@@ -1,4 +1,13 @@
 package addressprocessor.tests;
 
+import addressprocessor.utils.CsvUtil;
+
 public class PrintContentFile {
+
+    public static void main(String[] args) {
+
+        String fileName =  "tc_dir_dim2-temp";
+
+        CsvUtil.printCsvFile(fileName);
+    }
 }
