e52409f1ae2d2ebe934e6f7c638972a426027c9c
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-07-19 01:47:45 -0300

    feat: Create State Core DTO

---
+++ src/main/java/addressprocessor/dto/input/StateCoreDTO.java
@@ -0,0 +1,34 @@
+package addressprocessor.dto.input;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StateCoreDTO {
+
+    private String id;
+    private String name;
+    private String code;
+    private String countryCode;
+
+
+    public StateCoreDTO(String[] stateCoreLine){
+        this.id = stateCoreLine[1];
+        this.name = stateCoreLine[2];
+        this.code = stateCoreLine[5];
+        this.countryCode = stateCoreLine[3];
+    }
+
+    @Override
+    public String toString() {
+        return "StateCoreDTO{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", code='" + code + '\'' +
+                ", countryCode='" + countryCode + '\'' +
+                '}';
+    }
+}
